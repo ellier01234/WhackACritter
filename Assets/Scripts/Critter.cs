f0acd3b6a771b23d2f0da25ed1fa66d321af1212
ellier01234 <43067363+ellier01234@users.noreply.github.com>
2018-09-21 16:52:36 +0100

    Adding movement to sprites

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -12,15 +12,27 @@ public class Critter : MonoBehaviour {
 
     public int pointValue = 1; //how much is the critter worth?
 
+    private Vector2 direction; // what direction is the critter travelling in?  
+
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
             Random.Range(lowerRange.y, upperRange.y), 
             0);
-	}
+
+        // pick a random direction for this critter 
+        direction.x = Random.Range(-1.0f, 1.0f);
+        direction.y = Random.Range(-1.0f, 1.0f);
+        direction = direction.normalized;
+
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        transform.Translate(Vector2.up * Time.deltaTime);
+
 		if (timer.IsTimerRunning() == false)
         {
             Destroy(gameObject);
