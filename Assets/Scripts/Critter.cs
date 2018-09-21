b4eacafb97735714f0fc48673e510eeab3b33f57
ellier01234 <43067363+ellier01234@users.noreply.github.com>
2018-09-21 13:56:52 +0100

    Creating base critter functionality

---
+++ Assets/Scripts/Critter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Critter : MonoBehaviour {
+
+    public Vector2 lowerRange;
+    public Vector2 upperRange;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
+            Random.Range(lowerRange.y, upperRange.y), 
+            0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //unity calls this when the gameobject is clicked
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
